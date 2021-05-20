c66d081fd6467c0fadfffa3f3d8f595fc3271358
Binary gangsta <kirildalek111@gmail.com>
2021-05-20 11:44:16 +0300

    Added little functionallity to Play button and added a new activity GameActivity

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.tictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var name: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnPlay.setOnClickListener {
+            name = etName.text.toString()
+            if (name.isEmpty()) {
+                Toast.makeText(this, "Enter your name please)", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                intent = Intent()
+            }
+        }
 
     }
 }
